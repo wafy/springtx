93389e9c2a0503dad687d37ca21cac5f48aeee75
최선혁 <wafy75@gmail.com>
2022-10-16 15:35:12 +0900

    트랜잭션 전파 활용 - 전파 롤백

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -34,7 +34,7 @@ class MemberServiceTest {
         //when
         memberService.joinV1(username);
 
-        //then: 모든 데이터가 정상 저장된다.
+        //then: 로그데이타는 롤백된다.
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
     }
@@ -93,4 +93,25 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
     }
+
+    /**
+     * memberService  @Transactional: ON
+     * memberRepository  @Transactional: ON
+     * logRepository  @Transactional: ON Exception
+     */
+    @Test
+    void outerTxOn_fail() {
+        //given
+        String username = "로그예외_outerTxOn_fail";
+
+        //when
+        assertThatThrownBy(() ->
+                memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+        //then: 모든 데이터가 롤백된다.
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+
 }
